e3f4f9c36a7311b53a696a6f2cbe3f8184b0c680
ellenguo6 <ellenguo6@gmail.com>
2023-12-16 16:23:15 -0500

    better state managemetn

--- src/components/Stonks.js
+++ src/components/Stonks.js
@@ -6,6 +6,7 @@ import {
   YAxis,
   CartesianGrid,
   ReferenceLine,
+  ResponsiveContainer,
 } from "recharts";
 
 const Stonks = ({ data, notePoints, currentTime }) => {
@@ -28,30 +29,34 @@ const Stonks = ({ data, notePoints, currentTime }) => {
   let currentDatetimeStr = filteredData[currentTime]?.datetime_str;
 
   return (
-    <LineChart width={1000} height={300} data={data}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis
-        dataKey="datetime_str"
-        tickFormatter={formatDate}
-        minTickGap={25}
-        interval="preserveStart"
-      />
-      <YAxis type="number" domain={["auto", "auto"]} />
-      <Line
-        type="monotone"
-        dataKey="close"
-        stroke="#8884d8"
-        dot={{ fill: "#8884d8", r: 0 }}
-      />
-      <Line
-        connectNulls
-        type="monotone"
-        dataKey="note"
-        stroke="#82ca9d"
-        dot={{ fill: "#82ca9d", r: 4 }}
-      />
-      <ReferenceLine x={currentDatetimeStr} stroke="#82ca9d" />
-    </LineChart>
+    <div className="w-3/4">
+      <ResponsiveContainer height={300}>
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="datetime_str"
+            tickFormatter={formatDate}
+            minTickGap={25}
+            interval="preserveStart"
+          />
+          <YAxis type="number" domain={["auto", "auto"]} />
+          <Line
+            type="monotone"
+            dataKey="close"
+            stroke="#8884d8"
+            dot={{ fill: "#8884d8", r: 0 }}
+          />
+          <Line
+            connectNulls
+            type="monotone"
+            dataKey="note"
+            stroke="#82ca9d"
+            dot={{ fill: "#82ca9d", r: 4 }}
+          />
+          <ReferenceLine x={currentDatetimeStr} stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 
